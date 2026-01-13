f08bbb422b6ba558dc1bbc20d2f323003b556775
AntonIvanov <aivanovcustoms@yandex.ru>
2026-01-14 01:02:01 +0300

    Validation name, email and password

--- main.go
+++ main.go
@@ -3,27 +3,34 @@ package main
 import (
 	"errors"
 	"fmt"
-	"strings"
 )
 
 var (
-	errorLessTwoCharacters        = errors.New("consists of less than two characters")
-	errorLessThreeCharacters      = errors.New("consists of less than three characters")
-	errorNotLatAlphabetCharacters = errors.New("does not consist only of Latin alphabet characters")
-	errorMoreHundredCharacters    = errors.New("consists of more than hundred characters")
-	errorNotDog                   = errors.New("does not contain special character '@'")
+	errorLessTwoCharacters     = errors.New("consists of less than two characters")
+	errorLessThreeCharacters   = errors.New("consists of less than three characters")
+	errorLessEightCharacters   = errors.New("consists of less than eight characters")
+	errorMoreHundredCharacters = errors.New("consists of more than hundred characters")
+	errorNotDog                = errors.New("does not contain special character '@'")
+	errorCyrAlphabetCharacters = errors.New("cyrillic characters cannot be used")
+	errorNotDigit              = errors.New("there must be at least one digit")
+	errorNotLat                = errors.New("there must be at least one Latin character")
+	errorNotSpec               = errors.New("there must be at least one special character")
 )
 
 func validateName(validatedString string) error {
 	var errorTotal error
 	validateStringToRunes := []rune(validatedString)
+	counterCyr := 0
 	for _, rune := range validateStringToRunes {
-		if rune < 65 || rune > 122 || (rune <= 96 && rune >= 91) {
-			errorTotal = errorNotLatAlphabetCharacters
-			break
+		if rune == 1025 || rune == 1105 || (rune >= 1040 && rune <= 1103) {
+			counterCyr++
 		}
 	}
-	if len(validateStringToRunes) < 2 {
+	validateStringToRunesLenght := len(validateStringToRunes)
+	if counterCyr > 0 {
+		errorTotal = errorCyrAlphabetCharacters
+	}
+	if validateStringToRunesLenght < 2 {
 		if errorTotal != nil {
 			errorTotal = errors.Join(errorTotal, errorLessTwoCharacters)
 		} else {
@@ -39,19 +46,71 @@ func validateName(validatedString string) error {
 func validateEmail(validatedString string) error {
 	var errorTotal error
 	validateStringToRunes := []rune(validatedString)
+	counterCyr := 0
+	flagDog := false
 	for _, rune := range validateStringToRunes {
-		if rune < 65 || rune > 122 || (rune <= 96 && rune >= 91) {
-			if rune != 64 && rune != 46 {
-				errorTotal = errorNotLatAlphabetCharacters
-				break
-			}
+		if rune == 1025 || rune == 1105 || (rune >= 1040 && rune <= 1103) {
+			counterCyr++
+		} else if rune == 64 {
+			flagDog = true
 		}
 	}
-	if len(validateStringToRunes) < 3 {
+	validateStringToRunesLenght := len(validateStringToRunes)
+	if counterCyr > 0 {
+		errorTotal = errorCyrAlphabetCharacters
+	}
+	if validateStringToRunesLenght < 3 {
 		if errorTotal != nil {
 			errorTotal = errors.Join(errorTotal, errorLessThreeCharacters)
 		} else {
 			errorTotal = errorLessThreeCharacters
+		}
+	}
+	if validateStringToRunesLenght > 100 {
+		if errorTotal != nil {
+			errorTotal = errors.Join(errorTotal, errorMoreHundredCharacters)
+		} else {
+			errorTotal = errorMoreHundredCharacters
+		}
+	}
+	if !flagDog {
+		if errorTotal != nil {
+			errorTotal = errors.Join(errorTotal, errorNotDog)
+		} else {
+			errorTotal = errorNotDog
+		}
+	}
+	if errorTotal != nil {
+		return errorTotal
+	}
+	return nil
+}
+
+func validatePassword(validatedString string) error {
+	var errorTotal error
+	validateStringToRunes := []rune(validatedString)
+	counterLat := 0
+	counterDig := 0
+	counterCyr := 0
+	for _, rune := range validateStringToRunes {
+		if rune >= 48 && rune <= 57 {
+			counterDig++
+		} else if (rune >= 65 && rune <= 90) || (rune >= 97 && rune <= 122) {
+			counterLat++
+		} else if rune == 1025 || rune == 1105 || (rune >= 1040 && rune <= 1103) {
+			counterCyr++
+		}
+	}
+	validateStringToRunesLenght := len(validateStringToRunes)
+	counterSpec := validateStringToRunesLenght - (counterDig + counterLat + counterCyr)
+	if counterCyr > 0 {
+		errorTotal = errorCyrAlphabetCharacters
+	}
+	if validateStringToRunesLenght < 8 {
+		if errorTotal != nil {
+			errorTotal = errors.Join(errorTotal, errorLessEightCharacters)
+		} else {
+			errorTotal = errorLessEightCharacters
 		}
 	}
 	if len(validateStringToRunes) > 100 {
@@ -61,11 +120,25 @@ func validateEmail(validatedString string) error {
 			errorTotal = errorMoreHundredCharacters
 		}
 	}
-	if !strings.ContainsRune(validatedString, '@') {
+	if counterDig == 0 {
 		if errorTotal != nil {
-			errorTotal = errors.Join(errorTotal, errorNotDog)
+			errorTotal = errors.Join(errorTotal, errorNotDigit)
 		} else {
-			errorTotal = errorNotDog
+			errorTotal = errorNotDigit
+		}
+	}
+	if counterLat == 0 {
+		if errorTotal != nil {
+			errorTotal = errors.Join(errorTotal, errorNotLat)
+		} else {
+			errorTotal = errorNotLat
+		}
+	}
+	if counterSpec == 0 {
+		if errorTotal != nil {
+			errorTotal = errors.Join(errorTotal, errorNotSpec)
+		} else {
+			errorTotal = errorNotSpec
 		}
 	}
 	if errorTotal != nil {
@@ -80,6 +153,8 @@ func validate(mode, validatedString string) error {
 		return validateName(validatedString)
 	case "email":
 		return validateEmail(validatedString)
+	case "password":
+		return validatePassword(validatedString)
 	default:
 		return nil
 	}
@@ -89,11 +164,20 @@ func main() {
 	var modes = []string{"name", "email", "password"}
 	var names = []string{"", "a", "ю", "aa", "aю", "a.", "Azw", "Sw_r", "Dx2", "яя", "superuser"}
 	var emails = []string{
-		"user@mail.com", "", "u@m", "u@", "us", "qwertyuiopasdfghjkl",
+		"user@mail.com", "", "u@m", "u@", "us", "qwertyuiopasdfghjkl", "йцук", "йц@rtук", "йцукенг@",
+		"йцукенгшщзхъфывапро65", "йцукен:43", "qweйцук?0@fr", "12345678@er", "456789@йф", "иasdfg@12",
 		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxc",
 		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcv",
 		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm@wertyuiopasdfghjklzxc",
 		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnm@wertyuiopasdfghjklzxcv",
+	}
+	var passwords = []string{
+		"", "qwer", "qwertyui", "qwer1", "qwertyu1", "qwerф", "йцукqwer", "йцукен", "йцукенгш", "йцу1кенг",
+		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxc",
+		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcv",
+		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwerty1uiopasdfghjklzxcvbnmqwertyuiopasdfghjklzx",
+		"qwertyuiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxcvbnmqwerty1uiopasdfghjklzxcvbnmqwertyuiopasdfghjklzxc",
+		"qwer!6ty", "qwer4-#t", "qw45$e%r", "qыw67e^e",
 	}
 	for _, mode := range modes {
 		switch mode {
@@ -102,7 +186,7 @@ func main() {
 			for _, name := range names {
 				err := validate(mode, name)
 				if err != nil {
-					fmt.Printf("Error(s) in name %s: %s\n", name, err)
+					fmt.Printf("Error(s) in name %s:\n%s\n", name, err)
 				} else {
 					fmt.Println("Name:", name, "OK")
 				}
@@ -113,12 +197,23 @@ func main() {
 			for _, email := range emails {
 				err := validate(mode, email)
 				if err != nil {
-					fmt.Printf("Error(s) in email %s: %s\n", email, err)
+					fmt.Printf("Error(s) in email %s:\n%s\n", email, err)
 				} else {
 					fmt.Println("Email:", email, "OK")
 				}
 			}
 			fmt.Print("Email validation is completed", "\n\n")
+		case "password":
+			fmt.Println("Password validation started...")
+			for _, password := range passwords {
+				err := validate(mode, password)
+				if err != nil {
+					fmt.Printf("Error(s) in password %s:\n%s\n", password, err)
+				} else {
+					fmt.Println("Email:", password, "OK")
+				}
+			}
+			fmt.Print("Password validation is completed", "\n\n")
 		}
 	}
 }
